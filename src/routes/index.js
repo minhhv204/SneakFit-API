4d07b34b578df51fe402ac44bac21dfe00df6665
minhhv24 <hminh0555@gmail.com>
2024-10-17 21:48:48 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import categoriesRouter from "./categories.js";
 import productsRouter from "./products.js";
+import cartsRouter from "./carts.js";
 
 const router = Router();
 

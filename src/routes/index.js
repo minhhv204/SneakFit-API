f5c9257b29beaae969898393ec3d32cd47123e0e
phucnv050004 <phucvan050004@gmail.com>
2024-11-03 15:54:02 +0700

    conflic

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,7 @@ import { Router } from "express";
 import categoriesRouter from "./categories.js";
 import productsRouter from "./products.js";
 import cartsRouter from "./carts.js";
-<<<<<<< HEAD
 import authRouter from "./auth.js";
-=======
-import articlesRouter from "./article.js";
->>>>>>> ae7af23e91daf224be04a68d29185bd5e7ab0f70
 
 const router = Router();
 

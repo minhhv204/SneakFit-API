ac897fa7b6404285960709aa7577e6e0c42bfff8
phucnv050004 <phucvan050004@gmail.com>
2024-11-03 15:52:34 +0700

    auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,14 @@ import { Router } from "express";
 import categoriesRouter from "./categories.js";
 import productsRouter from "./products.js";
 import cartsRouter from "./carts.js";
+import authRouter from "./auth.js";
 
 const router = Router();
 
 router.get("/", (req, res) => {
   res.send("Home");
 });
-
+router.use("/auth", authRouter);
 router.use("/categories", categoriesRouter);
 router.use("/products", productsRouter);
 router.use("/carts", cartsRouter);
